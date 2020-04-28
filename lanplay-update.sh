1a67c57ba07b92ed07e8849856b0386329469364
Xuan-Bai <cpalm.org@gmail.com>
2020-04-28 08:31:14 +0800

    Create lanplay-update.sh

---
+++ lanplay-update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Writey by hsuanpai
+#Update lan-play-rpi of client from github.com
+#Date: 0428-2020, 08:30
+#Update: 0428-2020, 08:30
+#Email: cpalm{dot}org{AT}gmail{dot}com
+########################################
